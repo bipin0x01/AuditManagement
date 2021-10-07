12f6a2f5c00124efad26dc3d6d04238d020cc791
bipin0x01 <bt.kaji@gmail.com>
2021-10-07 21:31:52 +0545

    Added Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import LoginScreen from "./screens/LoginScreen.js";
 const App = () => {
   return (
-    <div>
-      I am app.js
-    </div>
-  )
-}
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/login" component={LoginScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/FormContainer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Row,Col, Container } from "react-bootstrap";
+
+const FormContainer = ({ children }) => {
+  return (
+    <div>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col xs={12} md={6}>
+            {children}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default FormContainer;
