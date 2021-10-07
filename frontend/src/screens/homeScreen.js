af45f6c859b9a0b39f71ef23478ba3d539654446
nishanprime <thapanishan9@gmail.com>
2021-10-08 01:50:33 +0545

    Lastest Push--Pre component

--- frontend/src/screens/homeScreen.js
+++ frontend/src/screens/homeScreen.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, Row } from 'react-bootstrap'
 const homeScreen = () => {
     return (
         <div>
-            
+            {/* <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row> */}
         </div>
     )
 }
