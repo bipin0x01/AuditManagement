4f98b0ec10b1fbb74f1d05adda5ab11102d1d9a9
nishanprime <thapanishan9@gmail.com>
2021-10-07 16:44:31 +0545

    Pre Seeder Script

---
+++ backend/data/auditData.js
@@ -0,0 +1,16 @@
+const auditFileData = [
+  {
+    clientName: "Nikhil Neupane",
+    auditFile: "http://localhost:5000/clientdata/sample1.png",
+  },
+  {
+    clientName: "Sangeet Ghimire",
+    auditFile: "http://localhost:5000/clientdata/sample2.pdf",
+  },
+  {
+    clientName: "Neruz Neupane",
+    auditFile: "http://localhost:5000/clientdata/sample3.docx",
+  },
+];
+
+export default auditFileData;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
-import express from "express"
+import express from "express";
+import path from "path";
 
-const app=express()
+const app = express();
+const __dirname = path.resolve();
+app.use(
+  "/clientdata",
+  express.static(path.join(__dirname, "/backend/uploads"))
+);
 
-app.get("/",(req,res)=>{
-    res.send("Kaji is gay")
-})
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.get("/", (req, res) => {
+  res.send("Kaji is gay");
+});
 
-app.listen(5000,()=>{
-    console.log("Listening on 5000")
-})
+app.listen(5000, () => {
+  console.log("Listening on 5000");
+});
