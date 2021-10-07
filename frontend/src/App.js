af737a4d2a837093547b8737383268fd1097677b
bipin0x01 <bt.kaji@gmail.com>
2021-10-08 03:20:54 +0545

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-<<<<<<< HEAD
 import LoginScreen from "./screens/LoginScreen";
-import HomeScreen from "./screens/HomeScreen";
-=======
-import loginScreen from "./screens/loginScreen";
 import HomeScreen from "./screens/HomeScreen"
 import ClientListScreen from "./screens/ClientList";
->>>>>>> a9312faf5391955e7af6c7994456b172f6e09c80
 const App = () => {
   return (
     <Router>
