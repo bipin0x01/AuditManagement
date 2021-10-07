608318746a8ddac763ba7bccb18d967a11f4d083
bipin0x01 <bt.kaji@gmail.com>
2021-10-08 02:39:21 +0545

    added card component for dashboard

--- frontend/src/screens/homeScreen.js
+++ frontend/src/screens/homeScreen.js
@@ -1,15 +1,9 @@
-import React from 'react'
-import { Card, Row } from 'react-bootstrap'
+import React from 'react';
+import DashCard from '../components/DashCard';
 const HomeScreen = () => {
     return (
         <div>
-            {/* <Row>
-            {products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row> */}
+            <DashCard title="as" value="10"/>
         </div>
     )
 }
