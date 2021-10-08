ae6aa6ced25e0e7bc5ac67b69569a6cf393679b0
bipin0x01 <bt.kaji@gmail.com>
2021-10-09 03:11:05 +0545

    dash fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,19 +16,14 @@ const App = () => {
     <Router>
       <Header />
           <Sidebar />
-        <div className="col-xl-9 col-lg-8 col-md-7 p-4 mainbody">
+      <div className="col-xl-9 col-lg-8 col-md-7 p-4 mainbody">
           <Route path="/login" component={LoginScreen} />
           <Route path="/" component={HomeScreen} exact />
           <Route path="/admin/clientlist" component={ClientListScreen} />
           <Route path="/admin/clients/:id/edit" component={ClientEditScreen} />
           <Route path="/admin/clients/:id/info" component={ClientScreen} />
-        </Container>
-      </main>
       <Footer />
-          <Route path="/admin/clientlist" component={ClientListScreen} exact />
-          <Footer/>
-        </div>
-      
+      </div>
     </Router>
   );
 };
