00dc48a29e87533867929b0bc9caeea82310dbd7
nishanprime <thapanishan9@gmail.com>
2021-10-09 03:14:36 +0545

    clientscreen start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,35 +4,40 @@ import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import LoginScreen from "./screens/LoginScreen";
-import HomeScreen from "./screens/HomeScreen"
+import HomeScreen from "./screens/HomeScreen";
 import ClientListScreen from "./screens/ClientList";
 import ClientEditScreen from "./screens/ClientEditScreen";
 import ClientScreen from "./screens/ClientScreen";
 import Sidebar from "./components/Sidebar";
-import './responsive.css'
+import "./responsive.css";
 
 const App = () => {
   return (
     <Router>
       <Header />
-        <div className="col-2 sidebar">
-          <Sidebar />
-        </div>
-        <div className="col-10 pt-4 mainbody">
-        <Container>
-          <Route path="/login" component={LoginScreen} />
-          <Route path="/" component={HomeScreen} exact />
-          <Route path="/admin/clientlist" component={ClientListScreen} />
-          <Route path="/admin/clients/:id/edit" component={ClientEditScreen} />
-          <Route path="/admin/clients/:id/info" component={ClientScreen} />
-        </Container>
-      </main>
-      <Footer />
-          <Route path="/admin/clientlist" component={ClientListScreen} exact />
+      <div className="col-2 sidebar">
+        <Sidebar />
+      </div>
+      <div className="col-10 pt-4 mainbody">
+        <main>
+          <Container>
+            <Route path="/login" component={LoginScreen} />
+            <Route path="/" component={HomeScreen} exact />
+            <Route path="/admin/clientlist" component={ClientListScreen} />
+            <Route
+              path="/admin/clients/:id/edit"
+              component={ClientEditScreen}
+            />
+            <Route path="/admin/clients/:id/info" component={ClientScreen} />
+            <Route
+              path="/admin/clientlist"
+              component={ClientListScreen}
+              exact
+            />
           </Container>
-          <Footer/>
-        </div>
-      
+        </main>
+      </div>
+      <Footer />
     </Router>
   );
 };
